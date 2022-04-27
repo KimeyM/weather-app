042cab6f131d25b4d9cbeb52dc7af4f7424ec138
KimeyM <kimey.-@hotmail.com>
2022-04-27 20:40:59 -0300

    listo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 import {BrowserRouter as Router} from 'react-router-dom';
-//"Router" es un alias, puede no estar e invocarlo con BrowserRouter
 
 ReactDOM.render(
   <Router>

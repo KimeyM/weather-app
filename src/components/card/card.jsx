cd938c55857a7661f4d6af48db54caa1f030f930
KimeyM <kimey.-@hotmail.com>
2022-04-27 20:11:29 -0300

    bug fixed

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import './card.css';
-import {Link} from "react-router-dom";
 
 export default function Card ({min, max, name, img, onClose, country, wind, weather, clouds, humidity, visibility, feelslike}) {
     return (
         <article>
-          <div class="containerCard">
-            <div class="card">
-                <div class="face face1">
-                    <div class="content">
+          <div className="containerCard">
+            <div className="card">
+                <div className="face face1">
+                    <div className="content">
                         <button className='btnClose' onClick={onClose}>x</button>
                         <h5 className='cardName'>{name}</h5>
                         <img src={"http://openweathermap.org/img/wn/"+img+"@2x.png"} width="100" height="100" alt="" />
                         <div className='cardMinMax'><p>Min {min}° </p><p> Max {max}°</p></div>
                     </div>
                 </div>
-                <div class="face face2">
-                    <div class="content">
+                <div className="face face2">
+                    <div className="content">
                     <p><span>Clima:</span> {weather}</p>
                     <p><span>Sensación térmica:</span> {feelslike}°C</p>
                     <p><span>Nubosidad:</span> {clouds}%</p>
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,7 +6,7 @@ import Logo from '../../img/logo.png';
 export default function Footer() {
   return (
     <footer>
-      <h3>Hecho por <br /> <a href="https://kimey-mastrangelo.vercel.app/" target="_blank" style={{color:'var(--darkblue)', fontWeight:'900', marginLeft:'5px'}}>Kimey Mastrángelo</a></h3>
+      <h3>Hecho por <br /> <a href="https://kimey-mastrangelo.vercel.app/" target="_blank"  rel="noreferrer" style={{color:'var(--darkblue)', fontWeight:'900', marginLeft:'5px'}}>Kimey Mastrángelo</a></h3>
       <img src={Logo} width="30" height="30" alt="" />
     </footer>
   )
